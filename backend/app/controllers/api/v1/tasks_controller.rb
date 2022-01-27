a48a49f35c3afff16b5db11970cac3c7679303e5
github username <altmanguitar@gmail.com>
2022-01-27 00:06:54 -0500

    tasks basic controller actions

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,44 @@
 class Api::V1::TasksController < ApplicationController
+    def index
+        tasks = Goal.find_by(id: params[:id]).tasks
+        render json: tasks
+    end
+
+    def show
+        task = Task.find(params[:id])
+        render json: task
+    end
+
+    def create
+        goal = Goal.find_by(id: params[:goal_id])
+        goal = goal.build(task_params)
+        if task.save
+            render json: task, status: :accepted
+       else
+           resp = {
+               error: task.errors.full_messages.to_sentence
+           }
+           render json: task, status: :unprocessable_entity
+       end
+    end
+
+    def update
+        task = Task.find_by(id: params[:id])
+        if task.update(task_params)
+            render json: task           
+       else
+           render json: task.errors, status: :unprocessable_entity
+       end
+    end
+
+    def destroy
+        task = Task.find(params[:id])
+        task.destroy
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:description, :goal_id)
+    end
 end
