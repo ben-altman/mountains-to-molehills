d858355415b1c0b8de8d9f5aac49773edf332140
github username <altmanguitar@gmail.com>
2022-01-26 21:17:12 -0500

    create dateline getter in log

--- backend/app/models/log.rb
+++ backend/app/models/log.rb
@@ -1,3 +1,8 @@
 class Log < ApplicationRecord
   belongs_to :project
+
+  def dateline
+    dateline = self.created_at.strftime('%A, %B %u')
+  end
+
 end
--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -3,4 +3,8 @@ class Project < ApplicationRecord
     has_many :tasks, through: :goals
     has_many :logs
 
+    def dateline
+        dateline = self.created_at.strftime('%A, %B %B, %Y')
+    end
+
 end
