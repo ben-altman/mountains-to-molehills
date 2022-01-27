7f966c70e90c9f111580f78112d115604bfbd071
github username <altmanguitar@gmail.com>
2022-01-27 00:04:26 -0500

    goals controller actions

--- backend/app/controllers/api/v1/goals_controller.rb
+++ backend/app/controllers/api/v1/goals_controller.rb
@@ -1,2 +1,45 @@
 class Api::V1::GoalsController < ApplicationController
+
+    def index
+        goals = Project.find_by(id: params[:id]).goals
+        render json: goals
+    end
+
+    def show
+        goal = Goal.find(params[:id])
+        render json: goal
+    end
+
+    def create
+        project = Project.find_by(id: params[:project_id])
+        goal = project.build(goal_params)
+        if goal.save
+            render json: goal, status: :accepted
+       else
+           resp = {
+               error: goal.errors.full_messages.to_sentence
+           }
+           render json: resp, status: :unprocessable_entity
+       end
+    end
+
+    def update
+        goal = Goal.find_by(id: params[:id])
+        if goal.update(goal_params)
+            render json: goal           
+       else
+           render json: goal.errors, status: :unprocessable_entity
+       end
+    end
+
+    def destroy
+        goal = Goal.find(params[:id])
+        goal.destroy
+    end
+
+    private
+
+    def goal_params
+        params.require(:goal).permit(:description, :project_id)
+    end
 end
--- backend/app/controllers/api/v1/projects_controller.rb
+++ backend/app/controllers/api/v1/projects_controller.rb
@@ -23,6 +23,7 @@ class Api::V1::ProjectsController < ApplicationController
     end
 
     def destroy
+        project = Project.find(params[:id])
         project.destroy
     end
 
