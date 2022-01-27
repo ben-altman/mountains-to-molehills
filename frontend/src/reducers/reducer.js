b97fa91283b8eb6281de4832114c8e0ccd7c40a6
github username <altmanguitar@gmail.com>
2022-01-27 18:31:11 -0500

    improve reducer stub

--- frontend/src/reducers/reducer.js
+++ frontend/src/reducers/reducer.js
@@ -1,7 +1,9 @@
-const reducer = (state = {}) => {
-
-    return state
-
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        
+        default: 
+            return state
+    }
 }
 
 export default reducer
